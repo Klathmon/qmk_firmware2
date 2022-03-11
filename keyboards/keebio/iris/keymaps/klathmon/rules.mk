77153ca1473bddefc8bc4f4ddf20a0782d8ed9fc
Gregory Benner <gregbenner1@gmail.com>
2022-03-11 10:55:21 -0500

    add my iris rev2 keymap

---
+++ keyboards/keebio/iris/keymaps/klathmon/rules.mk
@@ -0,0 +1,15 @@
+
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = no       	# Enable WS2812 RGB underlight.
+SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+ENCODER_ENABLE = no        # Enable rotary encoder
+OLED_ENABLE= no            # OLED display
+KEY_OVERRIDE_ENABLE = yes   # Enable key override
+LTO_ENABLE = yes            # Enable LTO
