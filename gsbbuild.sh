40e95e6b5302effa4a1a047795dd2b5c9d468143
Gregory Benner <gregbenner1@gmail.com>
2023-07-30 13:56:32 -0400

    add more of my used keymaps and keyboards and make a build directory with the output

--- gsbbuild.sh
+++ gsbbuild.sh
@@ -1,10 +1,20 @@
 #! /usr/bin/env bash
 
-rm -rf ./*.hex
+rm -rf ./*.hex ./build
 qmk clean
+
+qmk compile --parallel 0 -kb crkbd/rev1 -km default &
 qmk compile --parallel 0 -kb planck/rev3 -km klathmon &
+qmk compile --parallel 0 -kb planck/rev3 -km steno &
 qmk compile --parallel 0 -kb keebio/iris/rev2 -km klathmon &
 qmk compile --parallel 0 -kb keebio/bdn9/rev1 -km klathmon &
+qmk compile --parallel 0 -kb keebio/bdn9/rev1 -km default &
 qmk compile --parallel 0 -kb qkf4 -km klathmon &
 qmk compile --parallel 0 -kb lily58/light -km klathmon &
 qmk compile --parallel 0 -kb lily58/rev1 -km klathmon &
+
+mkdir -p ./build
+
+wait
+
+mv ./*.hex ./build
