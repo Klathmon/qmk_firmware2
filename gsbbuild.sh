0fc19b1869575b224ae03de279367222f14d4640
Gregory Benner <gregbenner1@gmail.com>
2023-07-29 20:20:05 -0400

    build in parallel

--- gsbbuild.sh
+++ gsbbuild.sh
@@ -2,9 +2,9 @@
 
 rm -rf ./*.hex
 qmk clean
-qmk compile --parallel 0 -kb planck/rev3 -km klathmon
-qmk compile --parallel 0 -kb keebio/iris/rev2 -km klathmon
-qmk compile --parallel 0 -kb keebio/bdn9/rev1 -km klathmon
-qmk compile --parallel 0 -kb qkf4 -km klathmon
-qmk compile --parallel 0 -kb lily58/light -km klathmon
-qmk compile --parallel 0 -kb lily58/rev1 -km klathmon
+qmk compile --parallel 0 -kb planck/rev3 -km klathmon &
+qmk compile --parallel 0 -kb keebio/iris/rev2 -km klathmon &
+qmk compile --parallel 0 -kb keebio/bdn9/rev1 -km klathmon &
+qmk compile --parallel 0 -kb qkf4 -km klathmon &
+qmk compile --parallel 0 -kb lily58/light -km klathmon &
+qmk compile --parallel 0 -kb lily58/rev1 -km klathmon &
